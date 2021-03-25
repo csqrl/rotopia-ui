07297fb3be65c33d968dab2fdca5bfdb3f170c49
Cam <21029087+ClockworkSquirrel@users.noreply.github.com>
2021-03-25 04:09:29 +0000

    VSCode debugger

--- src/Components/ClientAppLayers/ShopLayer/Navigation/NavButton.lua
+++ src/Components/ClientAppLayers/ShopLayer/Navigation/NavButton.lua
@@ -2,7 +2,7 @@ local Components = script.Parent.Parent.Parent.Parent
 local Packages = Components.Parent.Packages
 
 local Roact = require(Packages.Roact)
-local RoactRouterLink = require(Packages.RoactRouter.Link)
+local RoactRouter = require(Packages.RoactRouter)
 
 local e = Roact.createElement
 
@@ -19,46 +19,51 @@ Component.defaultProps = {
 }
 
 function Component:render()
-	return e(RoactRouterLink, {
-		path = self.props.path,
-		state = self.props.state,
-	}, {
-		e("ImageLabel", {
-			BackgroundColor3 = Color3.new(),
-			BackgroundTransparency = 1,
-			LayoutOrder = self.props.layoutOrder,
-			Size = UDim2.fromScale(1, 1),
-			Image = "rbxassetid://5038702297",
-			ImageColor3 = Color3.new(),
-			ImageRectSize = Vector2.new(512, 1024),
-			ScaleType = Enum.ScaleType.Slice,
-			SliceCenter = Rect.new(512, 512, 512, 512),
-			SliceScale = 6 / 512,
-		}, {
-			e("UIListLayout", {
-				Padding = UDim.new(),
-				FillDirection = Enum.FillDirection.Horizontal,
-				HorizontalAlignment = Enum.HorizontalAlignment.Center,
-				VerticalAlignment = Enum.VerticalAlignment.Center,
-			}),
-
-			e("UIPadding", {
-				PaddingBottom = UDim.new(0, 4),
-				PaddingLeft = UDim.new(0, 8),
-				PaddingRight = UDim.new(0, 8),
-				PaddingTop = UDim.new(0, 4),
-			}),
-
-			e("TextLabel", {
-				AutomaticSize = Enum.AutomaticSize.XY,
+	return e(RoactRouter.Context.Consumer, {
+		render = function(routing)
+			return e("ImageButton", {
+				BackgroundColor3 = Color3.new(),
 				BackgroundTransparency = 1,
-				Font = Enum.Font.GothamSemibold,
-				Text = self.props.label,
-				TextColor3 = Color3.new(1, 1, 1),
-				TextSize = 14,
-				TextWrapped = true,
-			}),
-		})
+				LayoutOrder = self.props.layoutOrder,
+				Size = UDim2.fromScale(1, 1),
+				Image = "rbxassetid://5038702297",
+				ImageColor3 = Color3.new(),
+				ImageRectSize = Vector2.new(512, 1024),
+				ScaleType = Enum.ScaleType.Slice,
+				SliceCenter = Rect.new(512, 512, 512, 512),
+				SliceScale = 6 / 512,
+
+				[Roact.Event.MouseButton1Click] = function()
+					routing.history:replace(
+						self.props.path, self.props.state
+					)
+				end,
+			}, {
+				e("UIListLayout", {
+					Padding = UDim.new(),
+					FillDirection = Enum.FillDirection.Horizontal,
+					HorizontalAlignment = Enum.HorizontalAlignment.Center,
+					VerticalAlignment = Enum.VerticalAlignment.Center,
+				}),
+
+				e("UIPadding", {
+					PaddingBottom = UDim.new(0, 4),
+					PaddingLeft = UDim.new(0, 8),
+					PaddingRight = UDim.new(0, 8),
+					PaddingTop = UDim.new(0, 4),
+				}),
+
+				e("TextLabel", {
+					AutomaticSize = Enum.AutomaticSize.XY,
+					BackgroundTransparency = 1,
+					Font = Enum.Font.GothamSemibold,
+					Text = self.props.label,
+					TextColor3 = Color3.new(1, 1, 1),
+					TextSize = 14,
+					TextWrapped = true,
+				}),
+			})
+		end,
 	})
 end
 
