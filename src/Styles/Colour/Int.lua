8898c2fc232104fd30f40c81e032c968875efbac
Cam <21029087+ClockworkSquirrel@users.noreply.github.com>
2021-03-26 14:55:27 +0000

    Fix Integer <-> Color3 conversion

--- src/Styles/Colour/Int.lua
+++ src/Styles/Colour/Int.lua
@@ -1,19 +1,31 @@
 local floor = math.floor
+local fmt = string.format
+
+local rshift = bit32.rshift
+local lshift = bit32.lshift
+local band = bit32.band
+
+local ERR_NOT_TYPE = "%s(...): The argument must be a %s, but you passed %q (%s)"
 
 local function IntToColor3(int: number): Color3
-    local red = floor(int / 256 ^ 2) % 256
-    local green = floor(int / 256) % 256
-    local blue = floor(int) % 256
+    assert(type(int) == "number", fmt(ERR_NOT_TYPE, "fromInt", "number", tostring(int), typeof(int)))
+    int = floor(int)
+
+    local red = band(rshift(int, 16), 255)
+    local green = band(rshift(int, 8), 255)
+    local blue = band(int, 255)
 
     return Color3.fromRGB(red, green, blue)
 end
 
 local function Color3ToInt(colour: Color3): number
-    local red = floor(colour.R * 255) * 256 ^ 2
-    local green = floor(colour.G * 255) * 256
-    local blue = floor(colour.B * 255)
+    assert(typeof(colour) == "Color3", fmt(ERR_NOT_TYPE, "toInt", "Color3", tostring(colour), typeof(colour)))
 
-    return red + green + blue
+    local int = floor(colour.R)
+    int = lshift(int, 8) + floor(colour.G)
+    int = lshift(int, 8) + floor(colour.B)
+
+    return int
 end
 
 return {
